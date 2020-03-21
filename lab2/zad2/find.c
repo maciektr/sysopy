fd7d989098934798bba244c4bd516aa4f6c5f900
Maciej <maciek2tr99@gmail.com>
2020-03-21 23:04:35 +0100

    Handle arguments

--- lab2/zad2/find.c
+++ lab2/zad2/find.c
@@ -19,19 +19,17 @@ void print_usage(){
     printf("    --atime n - file was last accessed n*24 hours ago. Negative n means at least, positive utmost.\n");
     printf("    --maxdepth n - descend  at  most  n  (a non-negative integer) levels\n");
 }
-/*
-mtime n
-File's  data was last modified n*24 hours ago.  See the comments for -atime to understand how round‐
-ing affects the interpretation of file modification times.
-atime n
-File was last accessed n*24 hours ago.  When find figures out how many 24-hour periods ago the  file
-was  last  accessed,  any fractional part is ignored, so to match -atime +1, a file has to have been
-accessed at least two days ago.
-maxdepth levels
-Descend  at  most  levels  (a non-negative integer) levels of directories below the starting-points.
--maxdepth 0
-means only apply the tests and actions to the starting-points themselves.
-*/
+
+void assert_args(bool x){
+    if(x) 
+        return;
+    print_usage(); 
+    exit(EXIT_FAILURE);
+}
+
+void find(char *path, char *name, int mtime, int atime, int maxdepth, bool nftw){
+    
+}
 
 int main(int argc, char *argv[]){
     static struct option long_options[] = {
@@ -47,51 +45,48 @@ int main(int argc, char *argv[]){
     int long_index = 0;
     int opt = 0;
 
-    int mtime;
-    int atime;
-    int maxdepth;
-    bool nftw;
-    
-    while ((opt = getopt_long_only(argc, argv,"hm:a:d:", long_options, &long_index )) != -1) {
-        // if (optarg)
-            // printf ("%d with arg %s\n",optind, optarg);
+    int mtime = 0;
+    int atime = 0;
+    int maxdepth = -1;
+    bool nftw = false;
+
+    while ((opt = getopt_long_only(argc, argv,"hnm:a:d:", long_options, &long_index )) != -1) {
         switch (opt) {
             case 'h':
                 print_usage();
             case 'm': 
-                    
+                assert_args(optarg != NULL);
+                mtime = atoi(optarg);
                 break;
             case 'a': 
-             
+                assert_args(optarg != NULL);
+                atime = atoi(optarg);
                 break;
             case 'd': 
-                if(optarg < 0){
-                    print_usage(); 
-                    exit(EXIT_FAILURE);
-                }
-                
+                assert_args(optarg > 0);
+                maxdepth = atoi(optarg);                
                 break;
-            default: print_usage(); 
-                exit(EXIT_FAILURE);
+            case 'n': 
+                nftw = true;
+                break;
+            default: 
+                assert_args(false);
         }
     }
-    // while(optind < argc){
-    //     printf("arg %s\n", argv[optind]);
-    //     optind++;
-    // }
+
     char *path = ".";
     char *name = NULL;
+
     if(optind+1 < argc){
         path = argv[optind];
         name = argv[optind+1];
     }else if(optind < argc){
         name = argv[optind];
     }else{
-        print_usage(); 
-        exit(EXIT_FAILURE);
+        assert_args(false);
     }
-    
 
+    find(path, name, mtime, atime, maxdepth, nftw);
 
     return 0;
 }
