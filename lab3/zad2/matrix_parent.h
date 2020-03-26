e470821fced5303ffa9bb1051de6e5a812ffed92
Maciej <maciek2tr99@gmail.com>
2020-03-26 20:32:25 +0100

    Add max / min defines

--- lab3/zad2/matrix_parent.h
+++ lab3/zad2/matrix_parent.h
@@ -6,6 +6,15 @@
 #include <string.h>
 #include <assert.h>
 
+#define max(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
+#define min(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+
 int count_lines(char *path);
 
 struct Task{
