7e628aa7bdf5f1e98fc10e1aa09515b4e73880c6
Maciej <maciek2tr99@gmail.com>
2020-03-26 20:31:54 +0100

    Add matrix struct

---
+++ lab3/zad2/matrix_worker.c
@@ -0,0 +1,84 @@
+#include "matrix_worker.h"
+
+Matrix *load_part(char *path, int col_min, int col_max, int row_min, int row_max){
+    assert(row_min <= row_max);
+    assert(col_min <= col_max);
+
+    if(row_min > row_max || col_min > col_max)
+        return NULL;
+
+    int n_rows = (row_max - row_min + 1);
+    int n_cols = (col_max - col_min +1);
+    int **matrix = malloc(n_rows * sizeof(int *));
+    for(int i = 0; i < n_rows; i++)
+        matrix[i] = malloc(n_cols * sizeof(int));
+
+    FILE *file = fopen(path, "r");
+    int act_row = 0, act_col = 0; 
+    int mat_row = 0, mat_col = 0;
+
+    char *line = NULL;
+    size_t len = 0;
+    while(getline(&line, &len, file) != -1){      
+        if(act_row < row_min){
+            act_row++;
+            continue;
+        }else if(act_row >= row_max)
+            break;
+
+        char *pch = strtok(line, " ");
+        while(pch != NULL){
+            if(act_col < col_min){
+                act_col++;
+                continue;
+            }else if(act_col >= col_max)
+                break;
+            
+            matrix[mat_row][++mat_col] = atoi(pch);
+
+            act_col++;
+            pch = strtok(NULL, " ");
+        }
+
+        act_row++;
+        mat_row++;
+        mat_col = 0; 
+    }
+    
+    free(line);
+    fclose(file);
+    Matrix *encl = malloc(sizeof(Matrix));
+    encl->matrix = matrix;
+    encl->n_rows = n_rows;
+    encl->n_cols = n_cols;
+    return encl;
+}
+
+Matrix *load_whole(char *path){
+    FILE *file = fopen(path, "r");
+    int n_cols = 0, n_rows = 0;
+
+    char *line;
+    size_t len;
+    int glstat = getline(&line, &len, file);
+    for(int i = 0; i<len; i++)
+        if(line[i] == ' ')
+            n_cols++;
+    n_cols++;
+    
+    if(glstat != -1){
+        n_rows=1;
+        while(getline(&line, &len, file) != -1)
+            n_rows++;
+    }
+
+    free(line);
+    fclose(file);
+    return load_part(path, 0, n_cols, 0, n_rows);
+}
+
+void free_matrix(Matrix *ptr){
+    while(--ptr->n_rows)
+        free(ptr->matrix[ptr->n_rows]);
+    free(ptr);
+}
--- lab3/zad2/matrix_worker.h
+++ lab3/zad2/matrix_worker.h
@@ -1,6 +1,19 @@
 #ifndef _MATRIX_WORKER_H
 #define _MATRIX_WORKER_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
 
+struct Matrix{
+    int **matrix;
+    int n_rows;
+    int n_cols;
+};
+typedef struct Matrix Matrix;
+Matrix *load_part(char *path, int col_min, int col_max, int row_min, int row_max);
+Matrix *load_whole(char *path);
+void free_matrix(Matrix *ptr);
 
 #endif
