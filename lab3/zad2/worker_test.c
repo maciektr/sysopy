738ba40439b30578e9c4da12ea1bc0ebfc0f5b71
Maciej <maciek2tr99@gmail.com>
2020-03-28 16:51:15 +0100

    Add Unity test

---
+++ lab3/zad2/worker_test.c
@@ -0,0 +1,16 @@
+#include "../../../../Unity/src/unity.h"
+#include "matrix_worker.h"
+
+void setUp(void) {
+}
+
+void tearDown(void) {
+}
+
+
+
+int main(void){
+    UNITY_BEGIN();
+    // RUN_TEST(test_function_should_doBlahAndBlah);
+    return UNITY_END();
+}
