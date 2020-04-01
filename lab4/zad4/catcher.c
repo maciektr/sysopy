7ff4ef38f78b18254dc594ea506cd1be9257bac8
Maciej <maciek2tr99@gmail.com>
2020-04-01 22:27:34 +0200

    Add lab4/zad4 only kill mode working

---
+++ lab4/zad4/catcher.c
@@ -0,0 +1,48 @@
+#include <linux/limits.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <assert.h>
+#include <string.h> 
+#include <stdio.h>
+
+int catched=0;
+
+void catcher_up(int sig){
+    catched++;
+    puts("XD");
+}
+
+void catch_end(int sig, siginfo_t * info, void *ucontext){
+    printf("Catched %d\n", catched);
+    while(catched--){
+        kill(info->si_pid, SIGUSR1);
+    }
+    kill(info->si_pid, SIGUSR2);
+    exit(EXIT_SUCCESS);
+}
+
+int main(){
+    printf("My pid: %d\n", (int)getpid());
+
+    sigset_t all;
+    sigfillset(&all);
+    sigdelset(&all, SIGUSR1);
+    sigdelset(&all, SIGUSR2);
+    sigdelset(&all, SIGINT);
+    assert(sigprocmask(SIG_BLOCK, &all, NULL) >= 0);
+
+    signal(SIGUSR1,catcher_up);
+    signal(SIGRTMIN,catcher_up);
+
+    struct sigaction act;
+    act.sa_flags = SA_SIGINFO;
+    act.sa_sigaction = &catch_end;
+    sigaction(SIGUSR2,&act, NULL);
+    sigaction(SIGRTMAX,&act, NULL);
+
+    while(1)
+        pause();
+}   
---
+++ lab4/zad4/sender.c
@@ -0,0 +1,73 @@
+#include <linux/limits.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <assert.h>
+#include <string.h> 
+#include <stdio.h>
+
+enum mode_t {kill_m, sigqueue_m, sigrt_m};
+
+void send(pid_t pid, int n, mode_t mode){
+    for(int i = 0; i<n; i++){
+        if(mode == kill_m)
+            kill(pid, SIGUSR1);
+        else if(mode == sigqueue_m){
+            union sigval s;
+            s.sival_int = SIGUSR1;
+            sigqueue(pid, 0, s);
+        }else if(mode == sigrt_m)
+            kill(pid, SIGRTMIN);
+    }
+    if(mode == kill_m)
+        kill(pid, SIGUSR2);
+    else if(mode == sigqueue_m){
+        union sigval s;
+        s.sival_int = SIGUSR2;
+        sigqueue(pid, 0, s);
+    }else if(mode == sigrt_m)
+            kill(pid, SIGRTMAX);
+}
+
+int catched = 0; 
+int n = 0;
+
+void catch_usr1(int sig){
+    catched++;
+}
+
+void catch_usr2(int sig){
+    printf("Sended: %d\nCatched %d\n", n, catched);
+    exit(EXIT_SUCCESS);
+}
+
+
+int main(int argc, char *argv[]){
+    assert(argc == 4);
+    int catcher_pid = atoi(argv[1]);
+    n = atoi(argv[2]);
+
+    char *mode_name = argv[3];
+    mode_t mode = kill_m; 
+    if(strcmp(mode_name, "SIGQUEUE") == 0)
+        mode = sigqueue_m;
+    else if(strcmp(mode_name, "SIGRT") == 0)
+        mode = sigrt_m;
+
+    sigset_t all;
+    sigfillset(&all);
+    sigdelset(&all, SIGUSR1);
+    sigdelset(&all, SIGUSR2);
+    sigdelset(&all, SIGINT);
+    assert(sigprocmask(SIG_BLOCK, &all, NULL) >= 0);
+
+    send((pid_t)catcher_pid, n, mode);
+
+    signal(SIGUSR1,catch_usr1);
+    signal(SIGUSR2,catch_usr2);
+    
+    while(1)
+        pause();
+}
