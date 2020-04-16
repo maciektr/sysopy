ec9d384b381025aae45a3e85afee744fb5fbddfd
maciektr <mtratnowiecki@gmail.com>
2020-04-16 22:59:18 +0200

    Create files for l5/z3

---
+++ lab5/zad3/manf.c
@@ -0,0 +1,13 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdbool.h> 
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(){
+    
+}
