26c290378d8dd285a20eb22e4c5a9cf78a32069c
maciektr <mtratnowiecki@gmail.com>
2020-04-16 23:05:55 +0200

    Read arguments

--- lab5/zad3/cons.c
+++ lab5/zad3/cons.c
@@ -8,6 +8,13 @@
 #include <string.h>
 #include <stdio.h>
 
-int main(){
+int main(int argc, char **argv){
+    assert(argc == 4);
+    char *fifo_path = argv[1];
+    char *file_path = argv[2];
+    int n_char = atoi(argv[3]);
+
+    FILE *file = fopen(file_path, "w");
     
+    fclose(file);
 }
