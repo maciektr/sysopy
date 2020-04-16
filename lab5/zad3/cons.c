8cb735a9bce3f2be560631e344a56eb520b13556
maciektr <mtratnowiecki@gmail.com>
2020-04-17 00:29:39 +0200

    Working manufacturer / consumer

--- lab5/zad3/cons.c
+++ lab5/zad3/cons.c
@@ -14,7 +14,14 @@ int main(int argc, char **argv){
     char *file_path = argv[2];
     int n_char = atoi(argv[3]);
 
+    FILE *fifo = fopen(fifo_path, "r");
     FILE *file = fopen(file_path, "w");
-    
+    char *buffer = malloc((n_char + 2) * sizeof(char));
+    int l;
+
+    while((l = fread(buffer, 1, n_char, fifo)) > 0)
+        fwrite(buffer, 1, l, file);
+
+    fclose(fifo);
     fclose(file);
 }
--- lab5/zad3/manf.c
+++ lab5/zad3/manf.c
@@ -7,14 +7,30 @@
 #include <assert.h>
 #include <string.h>
 #include <stdio.h>
+#include <time.h>
 
 int main(int argc, char **argv){
     assert(argc == 4);
+    srand(time(NULL));
     char *fifo_path = argv[1];
     char *file_path = argv[2];
     int n_char = atoi(argv[3]);
 
+    FILE *fifo = fopen(fifo_path, "a");
     FILE *file = fopen(file_path, "r");
-    
+    assert(fifo != NULL);
+    assert(file != NULL);
+    char *buffer = malloc((n_char + 2) * sizeof(char));
+    int l;
+
+    while((l = fread(buffer, 1, n_char, file)) > 0){
+        buffer[l] = '\0';
+        fprintf(fifo, "#%d#%s", (int)getpid(), buffer);
+        fflush(fifo);
+        sleep(rand()%3+1);
+    }
+
+    free(buffer);
+    fclose(fifo);
     fclose(file);
 }
