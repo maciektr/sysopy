1de73fd709780f48b672dc4000ff81065392e628
maciektr <mtratnowiecki@gmail.com>
2020-04-23 19:24:35 +0200

    Create common for sysV impl

--- lab6/chat_sysV/client.c
+++ lab6/chat_sysV/client.c
@@ -4,11 +4,15 @@
 #include <assert.h>
 #include <getopt.h>
 #include <stdio.h>
+#include <signal.h>
 
 #include <sys/msg.h>
 #include <sys/ipc.h>
 //- msgget, msgctl, msgsnd, msgrcv, ftok
 
-int main(int argc, char *argv[]){
+#include "common.h"
+
+int main() {
+//    SEND STOP ON CTRL+C
 
 }
---
+++ lab6/chat_sysV/common.h
@@ -0,0 +1,7 @@
+#ifndef _COMMON_H
+#define _COMMON_H
+
+//#define MAX_MSG_LEN
+#define PROJECT_ID 304529212
+
+#endif //_COMMON_H
