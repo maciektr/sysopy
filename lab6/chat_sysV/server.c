9d418263234b2806f61aab256d7c4e6af1925e77
maciektr <mtratnowiecki@gmail.com>
2020-04-23 19:25:26 +0200

    Register handlers

--- lab6/chat_sysV/server.c
+++ lab6/chat_sysV/server.c
@@ -1,14 +1,46 @@
+#include <sys/types.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
 #include <getopt.h>
+#include <unistd.h>
+#include <signal.h>
 #include <stdio.h>
+#include <pwd.h>
 
 #include <sys/msg.h>
 #include <sys/ipc.h>
 //- msgget, msgctl, msgsnd, msgrcv, ftok
 
-int main(int argc, char *argv[]){
+#include "common.h"
+void close_queue();
+void stop_sig();
+int queue_id = -1;
 
+int main(){
+    const char *homedir;
+    if ((homedir = getenv("HOME")) == NULL) {
+        homedir = getpwuid(getuid())->pw_dir;
+    }
+
+    atexit(close_queue);
+    signal(SIGINT,stop_sig);
+
+    key_t key = ftok(homedir, PROJECT_ID);
+    assert(key != -1);
+
+    queue_id = msgget(key, IPC_CREAT);
+    assert(queue_id != -1);
+
+}
+
+void stop_sig(){
+
+}
+
+void close_queue(){
+    if(queue_id == -1)
+        return;
+    assert(msgctl(queue_id, IPC_RMID, NULL) != -1);
 }
