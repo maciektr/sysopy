33bf8ec07b1c0d997e7bb71ba4688d9706086496
maciektr <mtratnowiecki@gmail.com>
2020-04-23 14:05:54 +0200

    Create lab6 files

---
+++ lab6/chat_sysV/client.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <getopt.h>
+#include <stdio.h>
+
+#include <sys/msg.h>
+#include <sys/ipc.h>
+//- msgget, msgctl, msgsnd, msgrcv, ftok
+
+int main(int argc, char *argv[]){
+
+}
---
+++ lab6/chat_sysV/main.c
@@ -0,0 +1,13 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <stdio.h>
+
+#include <sys/msg.h>
+#include <sys/ipc.h>
+//- msgget, msgctl, msgsnd, msgrcv, ftok
+
+int main(){
+
+}
