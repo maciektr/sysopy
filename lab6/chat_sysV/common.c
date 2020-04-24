4a651efb1e1ddcbc32ddd29bed0ecac827a0c364
maciektr <mtratnowiecki@gmail.com>
2020-04-24 23:37:25 +0200

    Add create_queue get_home helper functions

--- lab6/chat_sysV/common.c
+++ lab6/chat_sysV/common.c
@@ -1,1 +1,20 @@
 #include "common.h"
+
+
+#include <stdio.h>
+char *get_homedir() {
+    char *homedir;
+    if ((homedir = getenv("HOME")) == NULL) {
+        homedir = getpwuid(getuid())->pw_dir;
+    }
+    // printf("HOME: %s\n", homedir);
+    assert(homedir != NULL);
+    return homedir;
+}
+
+int create_queue(char *path, int id) {
+    key_t key = ftok(path, id);
+    assert(key != -1);
+    // printf("KEY %d\n", key);
+    return msgget(key, IPC_CREAT | QMOD);
+}
