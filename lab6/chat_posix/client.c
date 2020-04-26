ef19ee2486d314d36fbd4d2228d3e90bdf230bf4
maciektr <mtratnowiecki@gmail.com>
2020-04-26 19:07:54 +0200

    Add client init

--- lab6/chat_posix/client.c
+++ lab6/chat_posix/client.c
@@ -4,6 +4,7 @@
 #include <assert.h>
 #include <signal.h>
 #include <stdio.h>
+#include <time.h>
 
 #include <mqueue.h>
 // - mq_open, mq_send, mq_receive, mq_getattr, mq_setattr, mq_close, mq_unlink, mq_notify
@@ -23,6 +24,7 @@ void init();
 void print_help(int mode);
 
 int main(){
+    srand(time(NULL));
     init();
     print_help(0);
     // clear_stdin();
@@ -53,6 +55,80 @@ void print_help(int mode){
     }
 }
 
+char random_alpha(){
+    int n_upper = ('Z' - 'A');
+    int n_lower = ('z' - 'a');
+    int n_numb = ('9' - '0');
+    int N = n_numb + n_upper + n_lower;
+    int r = rand() % N;
+    if(r < n_numb)
+        return (char)(r);
+    if(r < n_numb + n_upper)
+        return (char)('A' + r);
+    return (char)('a' + r);
+}
+
+void get_clq_name(char *to, char *nick){
+    to[0] = '/';
+    to[1] = '\0';
+    strcat(to, nick);
+    int i = strlen(nick)+1;
+    to[i++] = '_';
+    while(i < CLQ_NAME_LEN)
+        to[i++] = random_alpha();
+}
+
+void init(){
+    atexit(atexit_handle);
+    signal(SIGINT, stop_sig);
+
+    srv_que = get_queue(SERVER_QUE_NAME, O_WRONLY);
+    assert(srv_que != -1);
+    
+    char nick[NICK_LEN];
+    puts("Please insert your nick.");
+    scanf("%s", nick);
+    while(strlen(nick) > NICK_LEN){
+        printf("Please insert valid nick. It can not be longer than %d\n", NICK_LEN);
+        scanf("%s", nick);
+    }
+
+    char clq_name[CLQ_NAME_LEN];
+    get_clq_name(clq_name, nick);
+    cl_que = get_queue(clq_name, O_RDONLY);
+    assert(cl_que != -1);
+
+    get_id(nick);
+}
+
+void get_id(char *nick){
+    // msg_t buffer;
+    // set_msg(&buffer, 0, INIT, cl_que);
+
+    // client cl;
+    // assert(strlen(nick) < NICK_LEN);
+    // strcpy(cl.nick, nick);
+    // cl.key = cl_que;
+    // cl.id = -1;
+    // buffer.clients[0] = cl;
+    
+    // assert(msgsnd(srv_que, &buffer, MSG_T_LEN, QMOD) != -1);
+    // assert(msgrcv(cl_que, &buffer, MSG_T_LEN, MSG_TYPE_URGENT, QMOD) != -1);
+    // my_id = buffer.integer_msg;
+    // assert(my_id != -1);
+}
+
+void atexit_handle() {
+    // msg_t buffer;
+    // set_msg(&buffer, my_id, STOP, 0);
+
+    // if(msgsnd(srv_que, &buffer, MSG_T_LEN, QMOD | IPC_NOWAIT) < 0)
+    //     exit(EXIT_FAILURE);
+
+
+    // if (cl_que != -1)
+    //     assert(msgctl(cl_que, IPC_RMID, NULL) != -1);
+}
 
 void stop_sig(){
     exit(EXIT_SUCCESS);
