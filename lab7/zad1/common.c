bcec84e442533336c253223e71b5de87d25b5783
maciektr <mtratnowiecki@gmail.com>
2020-04-29 21:00:39 +0200

    Add common function for shm mounting

--- lab7/zad1/common.c
+++ lab7/zad1/common.c
@@ -7,3 +7,22 @@ char *get_timestamp(){
     timeinfo = localtime ( &rawtime );
     return asctime(timeinfo);
 }
+
+char *get_homedir(){
+    char *homedir;
+    if ((homedir = getenv("HOME")) == NULL) {
+        homedir = getpwuid(getuid())->pw_dir;
+    }
+    assert(homedir != NULL);
+    return homedir;
+}
+
+int get_shm(){
+    key_t key = ftok(get_homedir(), PROJECT_ID);
+    assert(key != -1);
+    int shm = shmget(key, 0, 0);
+    if(shm != -1)
+        return shm;
+
+    return shmget(key, SHM_SIZE,IPC_CREAT);
+}
--- lab7/zad1/common.h
+++ lab7/zad1/common.h
@@ -1,8 +1,20 @@
 #ifndef _COMMON_H
 #define _COMMON_H
 
+#include <sys/shm.h> 
+#include <sys/ipc.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <assert.h>
 #include <time.h>
 
+#define PROJECT_ID 244
+#define ORDERS_N 1000
+#define SHM_SIZE (ORDERS_N * sizeof(int))
+
 char *get_timestamp();
+char *get_homedir();
+int get_shm();
 
 #endif //_COMMON_H
