1c25d941c6f84da0878d5def20983849ff0dec7e
maciektr <mtratnowiecki@gmail.com>
2020-04-29 21:33:13 +0200

    Add init

--- lab7/zad1/receiver.c
+++ lab7/zad1/receiver.c
@@ -13,6 +13,29 @@
 
 #include "common.h"
 
-int main(){
+int shm_id = -1;
+int lock_id = -1;
+void *shmaddr = NULL;
 
+int main(){
+    init();
+    
+}
+
+void init(){
+    atexit(atexit_handler);
+
+    shm_id = get_shm();
+    assert(shm_id != -1);
+    
+    shmaddr = shmat(shm_id,NULL,0);
+    assert(shmaddr != NULL);
+
+    lock_id = get_lock();
+    assert(lock_id != -1);
+}
+
+void atexit_handler(){
+    assert(shmdt(shmaddr) == 0);
+    shmctl(shm_id, IPC_RMID, NULL);
 }
