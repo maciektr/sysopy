34ecd072eafcbb454d3fade9da58c48fbea8bb9f
maciektr <mtratnowiecki@gmail.com>
2020-04-29 21:53:44 +0200

    Define shm_t structer for shared memory content

--- lab7/zad1/common.h
+++ lab7/zad1/common.h
@@ -11,12 +11,19 @@
 
 #define PROJECT_ID 244
 #define LOCK 164
-#define ORDERS_N 1000
+#define ORDERS_N 100
 #define SHM_SIZE (ORDERS_N * sizeof(int))
 #define QMOD 0666
 
 char *get_timestamp();
 char *get_homedir();
 int get_lock();
+
+typedef struct{
+    int orders[ORDERS_N];
+    int insert_index;
+    int pack_index;
+    int remove_index;
+} shm_t;
 
 #endif //_COMMON_H
