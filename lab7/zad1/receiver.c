4138b7c9898bfbe8e4ed63066447f4b13b66ef7c
maciektr <mtratnowiecki@gmail.com>
2020-04-30 14:08:02 +0200

    Register SIGINT handler, add simple routine

--- lab7/zad1/receiver.c
+++ lab7/zad1/receiver.c
@@ -15,20 +15,36 @@
 
 int shm_id = -1;
 int lock_id = -1;
-void *shmaddr = NULL;
+shm_t *shmaddr = NULL;
+
+void init();
+void atexit_handler();
+void sig_exit();
 
 int main(){
     init();
-    
+    while(1){
+        int val = rand() % 1000;
+        sem_qop(lock_id, -1);
+
+        shmaddr->orders[shmaddr->insert_index++] = val;
+        printf("(%d %s) Dodałem liczbę: %d. Liczba zamównień do przygotowania: %d. Liczba zamównień do wysłania: %d.", (int)getpid(), get_timestamp(), val, n_to_pack(shmaddr), n_to_send(shmaddr));
+
+        sem_qop(lock_id, 1);
+    }
 }
 
+
 void init(){
+    signal(SIGINT, sig_exit);
+
+    srand(time(NULL));
     atexit(atexit_handler);
 
     shm_id = get_shm();
     assert(shm_id != -1);
     
-    shmaddr = shmat(shm_id,NULL,0);
+    shmaddr = shmat(shm_id, NULL, 0);
     assert(shmaddr != NULL);
 
     lock_id = get_lock();
@@ -36,6 +52,11 @@ void init(){
 }
 
 void atexit_handler(){
+    assert(semctl(lock_id, 0, IPC_RMID) == 0);
     assert(shmdt(shmaddr) == 0);
     shmctl(shm_id, IPC_RMID, NULL);
+}
+
+void sig_exit(){
+    exit(EXIT_SUCCESS);
 }
