9724815ee2100c433b5b9032768887d09e9c8ff2
maciektr <mtratnowiecki@gmail.com>
2020-04-30 14:10:48 +0200

    Fix condition for IPC resource initialization

--- lab7/zad1/common.c
+++ lab7/zad1/common.c
@@ -1,5 +1,7 @@
 #include "common.h"
 
+#include<stdio.h>
+
 char *get_timestamp(){
     time_t rawtime;
     struct tm * timeinfo;
@@ -20,23 +22,34 @@ char *get_homedir(){
 int get_shm(){
     key_t key = ftok(get_homedir(), PROJECT_ID);
     assert(key != -1);
-    int shm = shmget(key, 0, QMOD);
-    if(shm != -1)
+    int shm = shmget(key, SHM_SIZE, IPC_CREAT | IPC_EXCL | QMOD);
+    if(shm != -1){
+        shm_t *shmaddr = (shm_t *)shmat(shm, NULL, 0);
+        shmaddr->insert_index = 0;
+        shmaddr->pack_index = 0;
+        shmaddr->remove_index = 0;
+        assert(shmdt(shmaddr) == 0);
         return shm;
-    // Memory is not yet created. 
-    shm = shmget(key, SHM_SIZE, IPC_CREAT | IPC_EXCL | QMOD);
-    shm_t *shmaddr = (shm_t *)shmat(shm, NULL, 0);
-    shmaddr->insert_index = 0;
-    shmaddr->pack_index = 0;
-    shmaddr->remove_index = 0;
-    assert(shmdt(shmaddr) == 0);
+    }
+
+    shm = shmget(key, 0, QMOD);
+    assert(shm != -1);
     return shm;
 }
 
 int get_lock(){
     key_t key = ftok(get_homedir(), LOCK);
     assert(key != -1);
-    return semget(key, 1, IPC_CREAT | QMOD);
+    
+    int sem = semget(key, 1, IPC_CREAT | IPC_EXCL | QMOD);
+    if(sem != -1){
+        sem_qop(sem, 1);
+        return sem; 
+    }
+    
+    sem = semget(key, 1, QMOD);
+    assert(sem != -1);
+    return sem;
 }
 
 void set_sembuff(sembuf_t *buffer, int num, int op, int flag){
