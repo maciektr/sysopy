a884858d919b12f427b9c84555ff63f6286c8de1
maciektr <mtratnowiecki@gmail.com>
2020-04-30 14:32:14 +0200

    Add simple sender routine

--- lab7/zad1/sender.c
+++ lab7/zad1/sender.c
@@ -13,6 +13,63 @@
 
 #include "common.h"
 
-int main(){
+int shm_id = -1;
+int lock_id = -1;
+shm_t *shmaddr = NULL;
 
+int shm_safe_rm(shm_t *shm);
+
+void init();
+void atexit_handler();
+void sig_exit();
+
+int main(){
+    init();
+    while(1){
+        sem_qop(lock_id, -1);
+
+        int r = shm_safe_rm(shmaddr);
+        if(r >= 0)
+            printf("(%d %s) Wysłałem zamówienie o wielkości: %d. Liczba zamównień do przygotowania: %d. Liczba zamównień do wysłania: %d.\n", (int)getpid(), get_timestamp(), r, n_to_pack(shmaddr), n_to_send(shmaddr));
+
+        sem_qop(lock_id, 1);
+    }
+}
+
+int shm_safe_rm(shm_t *shm){
+    int res = -1;
+    if(n_to_send(shm) > 0){
+        res = shm->orders[shm->remove_index % ORDERS_N] * 3;
+        shm->orders[shm->remove_index % ORDERS_N] = -1;
+        shm->remove_index++;
+        shm->remove_index %= ORDERS_N;
+    }
+    return res;
+}
+
+
+void init(){
+    signal(SIGINT, sig_exit);
+
+    srand(time(NULL));
+    atexit(atexit_handler);
+
+    shm_id = get_shm();
+    assert(shm_id != -1);
+    
+    shmaddr = shmat(shm_id, NULL, 0);
+    assert(shmaddr != NULL);
+
+    lock_id = get_lock();
+    assert(lock_id != -1);
+}
+
+void atexit_handler(){
+    assert(semctl(lock_id, 0, IPC_RMID) == 0);
+    assert(shmdt(shmaddr) == 0);
+    shmctl(shm_id, IPC_RMID, NULL);
+}
+
+void sig_exit(){
+    exit(EXIT_SUCCESS);
 }
