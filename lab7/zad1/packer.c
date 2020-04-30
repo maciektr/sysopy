fce6560a230918b7ecfb4e146e329c771fc171ec
maciektr <mtratnowiecki@gmail.com>
2020-04-30 14:24:15 +0200

    Simple packer routine

--- lab7/zad1/packer.c
+++ lab7/zad1/packer.c
@@ -13,6 +13,61 @@
 
 #include "common.h"
 
-int main(){
+int shm_id = -1;
+int lock_id = -1;
+shm_t *shmaddr = NULL;
 
+int shm_safe_pack(shm_t *shm);
+
+void init();
+void atexit_handler();
+void sig_exit();
+
+int main(){
+    init();
+    while(1){
+        sem_qop(lock_id, -1);
+
+        int r = shm_safe_pack(shmaddr);
+        if(r >= 0)
+            printf("(%d %s) Przygotowałem zamówienie o wielkości: %d. Liczba zamównień do przygotowania: %d. Liczba zamównień do wysłania: %d.\n", (int)getpid(), get_timestamp(), r, n_to_pack(shmaddr), n_to_send(shmaddr));
+
+        sem_qop(lock_id, 1);
+    }
+}
+
+int shm_safe_pack(shm_t *shm){
+    if(n_to_pack(shm) > 0){
+        shm->orders[shm->pack_index % ORDERS_N] *= 2;
+        shm->pack_index++;
+        return 0;
+    }
+    return -1;
+}
+
+
+void init(){
+    signal(SIGINT, sig_exit);
+
+    srand(time(NULL));
+    atexit(atexit_handler);
+
+    shm_id = get_shm();
+    assert(shm_id != -1);
+    
+    shmaddr = shmat(shm_id, NULL, 0);
+    assert(shmaddr != NULL);
+
+    lock_id = get_lock();
+    assert(lock_id != -1);
+}
+
+void atexit_handler(){
+    assert(semctl(lock_id, 0, IPC_RMID) == 0);
+    assert(shmdt(shmaddr) == 0);
+    shmctl(shm_id, IPC_RMID, NULL);
+}
+
+void sig_exit(){
+    exit(EXIT_SUCCESS);
 }
