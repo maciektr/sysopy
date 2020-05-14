15dbeeb9e0fdb8eec43fb5d53ed789a138329f3d
maciektr <mtratnowiecki@gmail.com>
2020-05-14 19:34:38 +0200

    Add read img function

--- lab8/zad1/main.c
+++ lab8/zad1/main.c
@@ -1,18 +1,22 @@
 #include <linux/limits.h>
 #include <sys/resource.h>
+#include <sys/types.h> 
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <sys/file.h>
 #include <sys/time.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
 #include <stdbool.h> 
 #include <getopt.h>
 #include <unistd.h> 
 #include <fcntl.h>
+#include <stdio.h>
 #include <time.h> 
-#include <stdlib.h>
-#include <assert.h>
-#include <string.h>
 
+int **read_image(char *filename, int *width, int *height);
+void free_img(int **img, int height);
 
 
 int main(int argc, char *argv[]){
@@ -23,5 +27,74 @@ int main(int argc, char *argv[]){
     char *file_in = argv[3];
     char *file_out = argv[4];
 
-    
+
+
+    int img_width, img_height;
+    int ** image = read_image(file_in, &img_width, &img_height);
+
+
+
+    free_img(image, img_height);
+}
+
+int **read_image(char *filename, int *width, int *height){
+    FILE *file = fopen(filename, "r");
+
+    char *line = NULL;
+    size_t len = 0; 
+    assert(getline(&line, &len, file) != -1);
+    assert(strcmp(line, "P2") == 0);
+
+    // Read width and height
+    while(getline(&line, &len, file) != -1)
+        if(line[0] == '#')
+            continue;
+        else{
+            char *pch = strtok(line, " "); 
+            assert(pch != NULL);
+            *width = atoi(pch);
+            pch = strtok(NULL, " ");
+            assert(pch != NULL);
+            *height = atoi(pch);
+            break;
+        }
+
+    // Allocate img
+    int **img = (int **)malloc((*height) * sizeof(int *));
+    for(int i = 0; i<height; i++)
+        img[i] = (int *)malloc((*width) * sizeof(int));
+
+    // Ignore the one line with max gray val
+    while(getline(&line, &len, file) != -1)
+        if(line[0] == '#')
+            continue;
+        else
+            break;
+
+    // Read img
+    int w=0,h=0;
+    while(getline(&line, &len, file) != -1){
+        if(line[0] == '#')
+            continue;
+        else{
+            char *pch = strtok(line, " ");
+            while(pch != NULL){
+                img[h][w] = atoi(pch);
+                w++;
+                pch = strtok(NULL," ");
+            }
+        }
+        w = 0;
+        h++;
+    }
+
+    free(line);
+    fclose(file);
+    return img;
+}
+
+void free_img(int **img, int height){
+    while(--height >= 0)
+        free(img[height]);
+    free(img);
 }
