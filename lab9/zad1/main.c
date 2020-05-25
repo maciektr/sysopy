689b032112ee42a3b40fdaf12529e91faf96d5e1
maciektr <mtratnowiecki@gmail.com>
2020-05-25 19:45:19 +0200

    Start implementation

--- lab9/zad1/main.c
+++ lab9/zad1/main.c
@@ -17,33 +17,77 @@
 #include <stdio.h>
 #include <time.h> 
 
-#define NEXT_CLIENT_WAIT 3
-#define JOB_WAIT 9
+#define SHORT_WAIT 5
+#define LONG_WAIT 10
 
 int n_clients;
 int n_chairs;
+int next_client;
+int next_chair;
+int used_chairs;
+int barber_sleeping;
 
 pthread_t *chairs;
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
-void barber_handler(){
-    while(1){
-
-    }
-    pthread_exit((void *)0);
-}
-
-void client_handler(){
-    while(1){
-        
-    }
-    pthread_exit((void *)0);
-}
-
 void wait_for_threads(int n_threads, pthread_t *threads);
 void random_sleep(int max);
+
+void *barber_handler(void *arg){
+    pthread_t shaved = 0;
+    int clients_left = n_clients;
+    while(1){
+        pthread_mutex_lock(&mutex);
+        if(used_chairs == 0){
+            printf("Golibroda: idzie spac.\n");
+            barber_sleeping = 1;
+            pthread_cond_wait(&cond, &mutex);
+            barber_sleeping = 0;
+        }else{
+            used_chairs--;
+            shaved = chairs[next_client++];
+            next_client %= n_chairs;
+            used_chairs--;
+        }
+        printf("Golibroda: goli klienta %ld.\nGolibroda: w poczekalni %d klientow.\n", shaved, used_chairs);
+        pthread_mutex_unlock(&mutex);
+        random_sleep(SHORT_WAIT);
+        pthread_mutex_lock(&mutex);
+        pthread_cancel(shaved);
+        clients_left--;
+        if(clients_left == 0)
+            break;
+        pthread_mutex_unlock(&mutex);
+    }
+    pthread_mutex_unlock(&mutex);
+    pthread_exit((void *)0);
+}
+
+void *client_handler(void *arg){
+    while(1){
+        pthread_mutex_lock(&mutex);
+        if(barber_sleeping){
+            printf("Klient %ld: budzi golibrode.\n", pthread_self());
+            pthread_cond_broadcast(&cond);
+            break;
+        }else if(used_chairs < n_chairs){
+            chairs[next_chair++] = pthread_self();
+            next_chair %= n_chairs;
+            used_chairs++;
+            printf("Klient %ld: siada w poczekalni.\n", pthread_self());
+        }else{
+            printf("Klient %ld: wszystkie krzesla zajete.\n",pthread_self());
+            pthread_mutex_unlock(&mutex);
+            random_sleep(SHORT_WAIT);
+            continue;
+        }
+        pthread_mutex_unlock(&mutex);
+    }
+    pthread_mutex_unlock(&mutex);
+    pthread_exit((void *)0);
+}
 
 int main(int argc, char *argv[]){
     assert(argc == 3);
@@ -54,10 +98,13 @@ int main(int argc, char *argv[]){
     n_clients = atoi(argv[2]);
 
     chairs = (pthread_t *)malloc(n_chairs * sizeof(pthread_t));
+    next_client = next_chair = used_chairs = 0;
+    barber_sleeping = 0;
+
     pthread_t *threads = (pthread_t *)malloc((n_clients + 1) * sizeof(pthread_t));
     pthread_create(&threads[0], NULL, barber_handler, NULL);
     for(int i = 1; i <= n_clients; i++){
-        random_sleep(NEXT_CLIENT_WAIT);
+        random_sleep(SHORT_WAIT);
         pthread_create(&threads[i], NULL, client_handler, NULL);
     }
 
