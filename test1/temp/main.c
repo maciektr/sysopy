e994d89d206c569357aa141021ca13b863a2114d
maciektr <mtratnowiecki@gmail.com>
2020-06-01 12:33:12 +0200

    Add test1 solutions

---
+++ test1/temp/main.c
@@ -0,0 +1,18 @@
+#include <ftw.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/times.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <dirent.h>
+
+int main(int argc, char *argv[]){
+    
+}
