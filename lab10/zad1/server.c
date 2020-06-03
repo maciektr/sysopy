757e274417c12a4b000de1a2e040900709fbf227
maciektr <mtratnowiecki@gmail.com>
2020-06-03 14:21:19 +0200

    Add run server

--- lab10/zad1/server.c
+++ lab10/zad1/server.c
@@ -9,6 +9,7 @@
 #include <stdbool.h> 
 #include <pthread.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <assert.h>
 #include <string.h>
 #include <getopt.h>
@@ -18,15 +19,70 @@
 #include <time.h> 
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 #include <sys/un.h>
 #include <netdb.h>
 
 #include "common.h"
 
+#define PING_SLEEP 10
+#define PING_TIMEOUT 5
+
+client *clients[MAX_CLIENTS];
+int active_clients = 0;
+
 int main(int argc, char *argv[]){
+    srand(time(NULL));
     assert(argc == 3);
 
     int port = atoi(argv[1]);
     char *socket_path = argv[2];
 
+    atexit(atexit_handle);
+    signal(SIGINT, stop_sig);   
+
+    
+}
+
+void run_server(char *path, int port){
+    struct sockaddr_un socket_unix;    
+    int socket_unix_fd = -1;
+
+    socket_unix.sun_family = AF_UNIX;
+    strcpy(socket_unix.sun_path, path);
+    socket_unix_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    assert(socket_unix_fd >= 0);
+    assert(bind(socket_unix_fd, (struct sockaddr *)&socket_unix, sizeof(socket_unix)) >= 0);
+    assert(listen(socket_unix_fd, MAX_CLIENTS) >= 0);
+    printf("Unix socket ready on: %s\n", path);
+
+    struct sockaddr_in socket_inet;
+    int socket_inet_fd = -1;
+
+    struct hostent* local_hostent = gethostbyname("localhost");
+    struct in_addr address = *(struct in_addr*) local_hostent->h_addr;
+
+    socket_inet.sin_family = AF_INET;
+    socket_inet.sin_port = htons(port);
+    socket_inet.sin_addr.s_addr = address.s_addr;
+
+    socket_inet_fd = socket(AF_INET, SOCK_STREAM, 0);
+    assert(socket_inet_fd  >= 0);
+    assert(bind(socket_inet_fd, (struct sockaddr*) &socket_inet, sizeof(socket_inet)) >= 0);
+    assert(listen(socket_inet_fd, MAX_CLIENTS) >= 0);
+    printf("INET socket ready on: %s:%d\n", inet_ntoa(address), port);
+}
+
+void atexit_handle() {
+    msg_t msg;
+    msg.type = CLOSE_GAME;
+    strcpy(msg.body, "Serwer rozlaczony.");
+    // for(int i = 0; i<active_clients; i++)
+        // send_msg(clients[i]., &msg);
+
+    //SHUTDOWN
+}
+
+void stop_sig(){
+    exit(EXIT_SUCCESS);
 }
