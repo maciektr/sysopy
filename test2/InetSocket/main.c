6a7abfcde31c2ccf2979a54375640b106d8d72b1
maciektr <mtratnowiecki@gmail.com>
2020-06-08 12:01:56 +0200

    Test 2

---
+++ test2/InetSocket/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+/*
+ * Funkcja 'ipconnect' przyjmuje:
+ *   - soket domeny AF_INET: 'sock',
+ *   - adres IP w postaci kropkowo dziesietnej: 'ip_addr',
+ *   - numer portu w lokalnym porzadku bajtow: 'port'.
+ *
+ * Funkcja 'ipconnect' powinna polaczyc soket 'sock' z
+ * adresem 'ip_addr' i portem 'port'. Funkcja powinna
+ * zwrocic 0 w przypadku sukcesu oraz -1 w przypadku bledua
+ * polaczenia.
+ */
+int ipconnect(int sock, char *ip_addr, short port) {
+    // Uzupelnij cialo funkcji ipconnect zgodnie z
+    // komentarzem powyzej.
+        struct sockaddr_in socket_addr;
+        socket_addr.sin_family = AF_INET;
+        socket_addr.sin_port = htons(port);
+        socket_addr.sin_addr.s_addr = inet_addr(ip_addr);
+
+        return connect(sock, (struct sockaddr*)&socket_addr, sizeof(socket_addr));
+}
+
+int main(int argc, char *argv[]) {
+    int sock;
+    int buf[32];
+    sock = socket(AF_INET , SOCK_STREAM , 0);
+    if(ipconnect(sock, "127.0.0.1", 7474)) return -1;
+    read(sock, buf, 32);
+    write(STDOUT_FILENO, buf, 32);
+    printf("\n");
+    shutdown(sock, SHUT_RDWR);
+    close(sock);
+    return 0;
+}
